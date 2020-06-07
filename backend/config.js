85583ba9bfc4e8bc77f1d95cb3a258fef2509004
sidharth-puri <sidharth171789.cse@chitkara.edu.in>
2020-06-07 17:51:34 +0530

    first

---
+++ backend/config.js
@@ -0,0 +1,9 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export default {
+  MONGODB_URL: process.env.MONGODB_URL ||  'mongodb+srv://dbUser:todoDB@cluster0-ku63s.mongodb.net/user?retryWrites=true',
+  JWT_SECRET: process.env.JWT_SECRET || 'somethingsecret',
+  PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID || 'sb',
+};
